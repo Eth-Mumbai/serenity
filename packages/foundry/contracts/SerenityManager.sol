68bf00ce680d685ab25fe609a42efb8d9ffed115
lordmike007 <manujasan23@gmail.com>
2024-03-31 05:26:10 +0530

    feat: create pool in createNewProtocol fn

--- packages/foundry/contracts/SerenityManager.sol
+++ packages/foundry/contracts/SerenityManager.sol
@@ -4,14 +4,21 @@ pragma abicoder v2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 import "./Serenity.sol";
 
 contract SerenityFactory {
     INonfungiblePositionManager public immutable nonFungiblePositionManager;
+    IUniswapV3Factory public immutable uniswapV3Factory;
     mapping(ERC20 => Serenity) public protocolSerenityContracts;
 
-    constructor(INonfungiblePositionManager nonFungiblePositionManager_) {
+    constructor(
+        INonfungiblePositionManager nonFungiblePositionManager_,
+        IUniswapV3Factory uniswapV3Factory_
+    ) {
         nonFungiblePositionManager = nonFungiblePositionManager_;
+        uniswapV3Factory = uniswapV3Factory_;
     }
 
     function createNewProtocol(
@@ -25,6 +32,17 @@ contract SerenityFactory {
             nonFungiblePositionManager,
             protocolName
         );
+
+        address pool_ = uniswapV3Factory.createPool(
+            address(token0),
+            address(token1),
+            500
+        );
+
+        IUniswapV3Pool pool = IUniswapV3Pool(pool_);
+
+        pool.initialize(79228162514264337593543950336); // initializing with price as 1 baseToken per quote
+
         protocolSerenityContracts[token0] = serenityContract;
     }
 }
